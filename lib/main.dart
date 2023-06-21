aa937d22feb583e9f9941a6945b6d5f5e86e63d4
R89 <r89@R89s-Mac-mini.local>
2023-06-21 10:18:28 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:animation_1/screen/view/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context) => HomeScreen(),
+      },
+    ),
+  );
+}
